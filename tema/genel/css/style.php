3f6475c86f87e6095ab1d5943f07365519fa820a
solitdio079 <solitdio079@gmail.com>
2026-01-14 15:28:01 +0300

    Solve new css issue

--- tema/genel/css/style.php
+++ tema/genel/css/style.php
@@ -101,6 +101,12 @@ h6 {
     height: 100vh;
     overflow: hidden;
   }
+  @media (max-width: 768px) {
+  .hero {
+    height: 60vh;
+    min-height: 380px;
+  }
+}
 
   .hero iframe {
     position: absolute;
